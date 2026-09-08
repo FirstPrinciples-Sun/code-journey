34275193a77fe545ae5fb53b3104c0e9b8da7861
FirstPrinciples-Sun <rtchanaphon@gmail.com>
2026-09-08 16:33:59 +0700

    Added: Section 2

--- java/Hello.java
+++ java/Hello.java
@@ -3,18 +3,8 @@ class Hello{
         System.out.println("Hello, World!");
 
         // Section:2
-        int num1 = 5;
-        byte by = 127;
-        short sh = 558;
-        long l = 5854l;
-
-        float f = 5.8f;
-        double d = 5.8;
-
-        char c = 'k';
-
-        boolean b = true;
-
-        System.out.println();
+        char c = 'a';
+        c++;
+        System.out.println(c);
     }
 }

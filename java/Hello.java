d6d77f28313869d4d5d0aa8676e3566c5a60acfa
FirstPrinciples-Sun <rtchanaphon@gmail.com>
2026-09-08 16:30:45 +0700

    Added: Section 1 Hello.java

---
+++ java/Hello.java
@@ -0,0 +1,20 @@
+class Hello{
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+
+        // Section:2
+        int num1 = 5;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+
+        System.out.println();
+    }
+}

29ae5c832a1e29f2f52314386d314f85d4b41c31
FirstPrinciples-Sun <rtchanaphon@gmail.com>
2026-09-08 16:42:20 +0700

    Added: Section 3 Typecasting

---
+++ java/TpyeCasting.java
@@ -0,0 +1,14 @@
+public class TpyeCasting {
+    public static void main(String[] args) {
+        // // byte b = 127;
+        // int a = 257;
+        // int k = (byte) a;
+       //  // System.out.println(k); // Output: 1
+
+        float f = 5.6f;
+        int t = (int) f;
+
+        System.out.println(t); // Output: 5
+
+    }
+}

8a424ad2284d46234e0bbce115d7d7899ced133d
FirstPrinciples-Sun <rtchanaphon@gmail.com>
2026-09-08 21:15:30 +0700

    Added: Increment

---
+++ java/Increment.java
@@ -0,0 +1,19 @@
+public class Increment {
+    public static void main(String[] args) {
+        // int num1 = 7;
+        // num1 += 2; // num1 + 2
+        // num1 *= 8; // num1 * 2
+
+        // System.out.println(num1); Output = 72
+
+        int num = 7;
+       // int result = num++; // fetch the value and then increment
+       // Output = 7
+        int result = ++num; // increment and the fretch the value
+        // Output = 8
+        System.out.println(result); 
+
+
+
+    }
+}
